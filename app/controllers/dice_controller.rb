69f3d9bf07626e973c1dfacdb63ca4555cdaae4d
Huni-Tuch <hunibert.tuch@kellogg.northwestern.edu>
2023-02-14 20:09:35 +0000

    passed some company and dice shit to backend

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -1,3 +1,14 @@
 class DiceController < ApplicationController
 
+    def index
+    
+        @die1 = rand(1..6)
+        @die2 = rand(1..6)
+        @total = @die1 + @die2
+        # using the @ makes the actual computing live in the backend, unobservable from the front en user
+        # these variablesonly live in the "dice" folder and "index" file
+    
+    end
+
+
 end
